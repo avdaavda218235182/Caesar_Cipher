f4c996a7e2c604b731b0845976e012bb8b390a09
Yonatan avda <avday@yatmal.co.il>
2025-11-05 15:31:33 +0200

    the function caesar_encript works, need to make it word with the other files

--- src/caesar_encrypt.py
+++ src/caesar_encrypt.py
@@ -1,6 +1,15 @@
 # main.py
 def caesar_encrypt(text, shift):
     result = ""
+    char = ''
+    for i in text:
+        if i.isalpha() and i.islower():
+            char = chr(range(ord('a'), ord('z') + 1)[(ord(i) - ord('a') + shift) % 26])
+        elif i.isalpha() and i.isupper():
+            char = chr(range(ord('A'), ord('Z') + 1)[(ord(i) - ord('A') + shift) % 26])
+        else:
+            char = i
+        result += char
     return result
 
 if __name__ == "__main__":
